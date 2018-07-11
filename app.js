dc4e949e22a3ab99786f9d54a54f8e0822fbc7e0
Wink <410407346@qq.com>
2018-07-11 20:20:43 +0800

    shuffle adaption

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch: function() {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -14,38 +14,40 @@ App({
     })
     // 获取用户信息
     wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            wx.getUserInfo({
+              success: res => {
+                // 可以将 res 发送给后台解码出 unionId
+                this.globalData.userInfo = res.userInfo
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                // 所以此处加入 callback 以防止这种情况
+                if (this.userInfoReadyCallback) {
+                  this.userInfoReadyCallback(res)
+                }
               }
-            }
-          })
+            })
+          }
         }
-      }
-    }),
-    wx.getSystemInfo({
-      success: res => {
-        this.globalData.clientHeight = res.windowHeight
-        if (this.SystemInfoCallback) {
-          this.SystemInfoCallback(res)
-        } console.log(this.globalData)
-      }
-    })
+      }),
+      wx.getSystemInfo({
+        success: res => {
+          this.globalData.clientHeight = res.windowHeight;
+          this.globalData.clientWidth = res.windowWidth;
+          if (this.SystemInfoCallback) {
+            this.SystemInfoCallback(res)
+          }
+        }
+      })
   },
-  onShow: function (options) {
+  onShow: function(options) {
 
   },
   globalData: {
     userInfo: null,
-    clientHeight: ''
+    clientHeight: '',
+    clientWidth: ''
   }
 })
--- pages/createImg/createImg.js
+++ pages/createImg/createImg.js
@@ -1,26 +1,53 @@
+var app = getApp();
 Page({
   data: {
     prurl: '',
-    hidden: true
+    hidden: true,
+    clientHeight: '',
+    clientWidth: ''
   },
   onLoad: function() {
+    this.setData({
+      clientHeight: app.globalData.clientHeight - 50,
+      clientWidth: app.globalData.clientWidth - 2
+    });
     this.init();
   },
-  init: function(){
-    this.shuffle();
+  init: function() {
+    this.shuffle(); //洗牌
   },
-  shuffle: function(){
+  shuffle: function() {
     const ctx = wx.createCanvasContext('shuffle');
-    for (var i = 0; i < 1; i++) {
-      var x = Math.random() * 270;
-      var y = Math.random() * 270;
-      var rx = x + 50 / 2;
-      var ry = y + 92 / 2;
-      console.log(x, y, rx, ry)
-      ctx.translate(rx, ry);
-      ctx.rotate(20 * Math.PI / 180);
-      ctx.drawImage('../../image/waiteTarot/78.jpg', x + 30, y + 30, 50, 92);
 
+    for (var i = 0; i < 78; i++) {
+      var x = 0,
+        y = 0,
+        deg = 0,
+        rx = 0,
+        ry = 0;
+      if (this.data.clientWidth < 350) {
+        x = Math.random() * 160 + 50;
+        y = Math.random() * 160 + 15;
+        deg = Math.random() * Math.PI;
+        rx = x + 50;
+        ry = y + 46;
+        ctx.translate(rx, ry);
+        ctx.rotate(deg);
+        ctx.drawImage('../../image/waiteTarot/78.jpg', 0, 0, 35, 62);
+        ctx.rotate(-deg);
+        ctx.translate(-rx, -ry);
+      } else {
+        x = Math.random() * 190 + 50;
+        y = Math.random() * 200 + 20;
+        deg = Math.random() * Math.PI;
+        rx = x + 50;
+        ry = y + 46;
+        ctx.translate(rx, ry);
+        ctx.rotate(deg);
+        ctx.drawImage('../../image/waiteTarot/78.jpg', 25, -46, 45, 80);
+        ctx.rotate(-deg);
+        ctx.translate(-rx, -ry);
+      }
     }
     ctx.draw()
   },
