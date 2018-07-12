2be9f4660abbfc56ccdf5dc6fbb1110c52763421
Wink <410407346@qq.com>
2018-07-12 19:11:17 +0800

    chou

--- pages/test/test.js
+++ pages/test/test.js
@@ -2,30 +2,15 @@ var order = ['red', 'yellow', 'blue', 'green', 'red']
 Page({
   data: {
     toView: 'red',
-    scrollTop: 100
+    scrollTop: 100,
+    top: '',
+    left: ''
   },
-  upper: function (e) {
-    console.log(e)
-  },
-  lower: function (e) {
-    console.log(e)
-  },
-  scroll: function (e) {
-    console.log(e)
-  },
-  tap: function (e) {
-    for (var i = 0; i < order.length; ++i) {
-      if (order[i] === this.data.toView) {
-        this.setData({
-          toView: order[i + 1]
-        })
-        break
-      }
-    }
-  },
-  tapMove: function (e) {
+  drag: function(e) {
+    console.log(e.touches[0].clientX)
     this.setData({
-      scrollTop: this.data.scrollTop + 10
+      left:e.touches[0].clientX - 25,
+      top:e.touches[0].clientY - 25
     })
   }
 })
