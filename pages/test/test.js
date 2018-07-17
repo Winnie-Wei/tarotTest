57ad0027d3f24f375bbea787dff6c0f805e5a6e6
WinnieWei <410407346@qq.com>
2018-07-17 23:39:25 +0800

    img

--- pages/test/test.js
+++ pages/test/test.js
@@ -11,16 +11,12 @@ Page({
     prurl: ''
   },
   onLoad: function() {
-    const ctx = wx.createCanvasContext('poker');
-    ctx.drawImage('../../image/waiteTarot/78.jpg', 200, 0, 90, 150);
     this.setData({
       clientHeight: app.globalData.clientHeight - 150,
       clientWidth: app.globalData.clientWidth - 4
     });
   },
   drag: function(e) {
-    console.log(e)
-
     this.setData({
       left: e.touches[0].clientX - 45,
       top: e.touches[0].clientY - 75,
@@ -28,39 +24,15 @@ Page({
     })
   },
   share: function() {
-    var that = this;
-    const ctx = wx.createCanvasContext('poker');
-    ctx.drawImage('../../image/waiteTarot/78.jpg', 200, 0, 90, 150);
-    getImg().then(function(data) {
-      // console.log(data)
-      // data.forEach(function(item) {
-      //   var imgUrl = '../../image/waiteTarot/' + item.id + '.jpg'
-      //   ctx.drawImage(imgUrl, item.left, item.top, item.width, item.height);
-      // });
-      ctx.drawImage('../../image/waiteTarot/78.jpg', 200, 0, 90, 150);
-
-    //   wx.canvasToTempFilePath({
-    //     x: 0,
-    //     y: 0,
-    //     width: that.data.clientWidtht,
-    //     height: that.data.clientHeight,
-    //     destWidth: that.data.clientWidtht,
-    //     destHeight: that.data.clientHeight,
-    //     canvasId: 'img',
-    //     success: function(res) {
-    //       console.log(res.tempFilePath);
-    //       that.setData({
-    //         prurl: res.tempFilePath
-    //       })
-    //     },
-    //     fail: function(res) {
-    //       console.log(res)
-    //     }
-    //   })
+    //var that = this;
+    getImg().then(data => {
+      drawImg(data);
+    }).then(() => {
+      this.createImg();
     })
   },
   save: function() {
-    var that = this
+    var that = this;
     wx.saveImageToPhotosAlbum({
       filePath: that.data.prurl,
       success(res) {
@@ -72,19 +44,36 @@ Page({
           success: function(res) {
             if (res.confirm) {
               console.log('用户点击确定');
-              /* 该隐藏的隐藏 */
-              that.setData({
-                hidden: true
-              })
             }
           }
         })
       }
     })
+  },
+  createImg: function(){
+    var that = this;
+    wx.canvasToTempFilePath({
+      x: 0,
+      y: 0,
+      width: that.data.clientWidtht,
+      height: that.data.clientHeight,
+      destWidth: that.data.clientWidtht,
+      destHeight: that.data.clientHeight,
+      canvasId: 'img',
+      success: function (res) {
+        console.log(res.tempFilePath);
+        that.setData({
+          prurl: res.tempFilePath
+        })
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
   }
 });
 
-function getImg() {
+function getImg(that) {
   var imgList = [];
   return new Promise((resolve, reject) => {
     wx.createSelectorQuery().selectAll('.imgcss.t').boundingClientRect(function(rect) {
@@ -93,5 +82,18 @@ function getImg() {
       });
       resolve(imgList);
     }).exec();
+  })
+}
+
+function drawImg(data) {
+  const ctx = wx.createCanvasContext('img');
+  return new Promise((resolve, reject) => {
+    data.forEach(function(item) {
+      var imgUrl = '../../image/waiteTarot/' + item.id + '.jpg';
+      ctx.drawImage(imgUrl, item.left, item.top, item.width, item.height);
+      ctx.drawImage('../../image/waiteTarot/78.jpg', item.left + 20, item.top + 20, item.width, item.height);
+    });
+    ctx.draw();
+    resolve();
   })
 }
