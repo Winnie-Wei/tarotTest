4a0730b6e26ea59515f6bd7e6f7be877dff7f0fb
Wink <410407346@qq.com>
2018-07-19 19:59:54 +0800

    test img

--- pages/test/test.js
+++ pages/test/test.js
@@ -11,7 +11,8 @@ Page({
     left: '',
     imgList: [],
     isChecked: '',
-    prurl: ''
+    prurl: '',
+    cardArr: []
   },
   onLoad: function() {
     this.setData({
@@ -20,19 +21,32 @@ Page({
     });
   },
   drag: function(e) {
-    this.setData({
-      left: e.touches[0].clientX - 45,
-      top: e.touches[0].clientY - 75,
-      isChecked: e.currentTarget.id
-    })
+    console.log(e)
+    var card = extendObj(e.currentTarget, e.touches[0]);
+    this.data.cardArr.push(card);
+    var hash = {};
+    this.data.cardArr = this.data.cardArr.reduce(function(item, next) { //去除重复项
+      hash[next.id] ? '' : hash[next.id] = true && item.push(next);
+      return item
+    }, []);
+    console.log(this.data.cardArr)
+    // this.setData({
+    //   left: e.touches[0].clientX - 45,
+    //   top: e.touches[0].clientY - 75,
+    //   isChecked: e.currentTarget.id
+    // });
+    // var imgUrl = '../../image/waiteTarot/' + e.currentTarget.id + '.jpg';
+    // ctx.drawImage(imgUrl, this.data.left, this.data.top, 90, 150);
+    // ctx.draw();
   },
   share: function() {
     //var that = this;
-    getImg().then(data => {
-      drawImg(data);
-    }).then(() => {
-      this.createImg();
-    })
+    // getImg().then(data => {
+    //   drawImg(data);
+    // }).then(() => {
+    //   this.createImg();
+    // })
+    this.createImg();
   },
   save: function() {
     var that = this;
@@ -53,7 +67,7 @@ Page({
       }
     })
   },
-  createImg: function(){
+  createImg: function() {
     var that = this;
     wx.canvasToTempFilePath({
       x: 0,
@@ -63,13 +77,13 @@ Page({
       destWidth: that.data.clientWidtht,
       destHeight: that.data.clientHeight,
       canvasId: 'img',
-      success: function (res) {
+      success: function(res) {
         console.log(res.tempFilePath);
         that.setData({
           prurl: res.tempFilePath
         })
       },
-      fail: function (res) {
+      fail: function(res) {
         console.log(res)
       }
     })
@@ -90,15 +104,36 @@ function getImg(that) {
 
 function drawImg(data) {
   return new Promise((resolve, reject) => {
-    ctx.clearRect(0, 0, clientWidth, clientHeight);
-    ctx.draw();
-    // data.forEach(function(item) {
-    //   var imgUrl = '../../image/waiteTarot/' + item.id + '.jpg';
-    //   ctx.drawImage(imgUrl, item.left, item.top, item.width, item.height);
-    //   ctx.drawImage('../../image/waiteTarot/78.jpg', item.left + 20, item.top + 20, item.width, item.height);
-    // });
-    ctx.drawImage('../../image/waiteTarot/78.jpg', 20,20, 150, 150);
-    ctx.draw(true);
+    // ctx.clearRect(0, 0, clientWidth, clientHeight);
+    // ctx.draw();
+    // // data.forEach(function(item) {
+    // //   var imgUrl = '../../image/waiteTarot/' + item.id + '.jpg';
+    // //   ctx.drawImage(imgUrl, item.left, item.top, item.width, item.height);
+    // //   ctx.drawImage('../../image/waiteTarot/78.jpg', item.left + 20, item.top + 20, item.width, item.height);
+    // // });
+    // ctx.drawImage('../../image/waiteTarot/78.jpg', 20, 20, 150, 150);
+    // ctx.draw(true);
     resolve();
   })
+}
+
+function cloneObj(oldObj) { //复制对象方法
+  if (typeof(oldObj) != 'object') return oldObj;
+  if (oldObj == null) return oldObj;
+  var newObj = new Object();
+  for (var i in oldObj)
+    newObj[i] = cloneObj(oldObj[i]);
+  return newObj;
+};
+
+function extendObj() { //扩展对象
+  var args = arguments;
+  if (args.length < 2) return;
+  var temp = cloneObj(args[0]); //调用复制对象方法
+  for (var n = 1; n < args.length; n++) {
+    for (var i in args[n]) {
+      temp[i] = args[n][i];
+    }
+  }
+  return temp;
 }
